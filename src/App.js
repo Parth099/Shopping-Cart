a22194df2c25c3d91ed3f037b463b2ca31b68193
Parth099 <parthnpatel715@gmail.com>
2022-05-07 15:42:02 -0400

    make conditional render more efficient. New fix ignores compo instead of making it invis

--- src/App.js
+++ src/App.js
@@ -37,18 +37,20 @@ function App() {
                     </div>
                 </div>
             </div>
-            <div className={"Landing Page text-white " + (location.pathname === "/" ? "" : "void")}>
-                <section>
-                    <div className="home-page-curve">
-                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                            <path
-                                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                                className="shape-fill"
-                            ></path>
-                        </svg>
-                    </div>
-                </section>
-            </div>
+            {location.pathname === "/" /*render if we are at home ONLY*/ && (
+                <div className="Landing Page text-white">
+                    <section>
+                        <div className="home-page-curve">
+                            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                                <path
+                                    d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                                    className="shape-fill"
+                                ></path>
+                            </svg>
+                        </div>
+                    </section>
+                </div>
+            )}
             <div className="mx-12">
                 <Outlet />
             </div>
