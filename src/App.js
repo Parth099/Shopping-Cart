63e239138468fca6afe00e40864b843fa226f31b
Parth099 <parthnpatel715@gmail.com>
2022-05-08 20:10:17 -0400

    added in images of phones moved the landing page into its own functional compo and set that as an index route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import useCart from "./hooks/useCart";
 
 //image
 import bagIcon from "./img/icon/bag.png";
-import coverPhone from "./img/phones/samsung/galaxy_z_flip3.jpg";
+
 
 //mostly nav bar logic
 function App() {
@@ -34,33 +34,6 @@ function App() {
                     </div>
                 </div>
             </div>
-            {currRoute.pathname === "/" /*render if we are at home ONLY*/ && (
-                <div className="Landing Page text-white relative min-h-screen">
-                    <section className="opening-ad">
-                        <div className="w-rbox border-white border-4 mx-auto mt-10 rounded-lg z-10 md:w-128 sm:w-96 xs:w-64">
-                            <img src={coverPhone} alt="Galaxy Flip 3" className="w-100 h-100" />
-                            <div className="bg-white flex flex-col text-slate-600 p-8 text-2xl sm:text-xl xs:text-sm">
-                                <p className="text-sm mb-8">
-                                    Unexpected colors. A new protective layer and film to fortify the main screen. An IPX8 water resistance that lets
-                                    you post even in a downpour. If it sounds like we rethought the Flip, we did. Now it's a statement.
-                                </p>
-                                <p class="descrip">
-                                    <span class="prompt-font">Name</span>
-                                    <p>Galaxy Z Flip3</p>
-                                </p>
-                                <p class="descrip">
-                                    <span class="prompt-font">Price</span>
-                                    <p>$999.99</p>
-                                </p>
-                                <p class="descrip">
-                                    <span class="prompt-font">Storage</span>
-                                    <p>256 TB</p>
-                                </p>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            )}
             <div className="mx-12">
                 <Outlet />
             </div>
--- src/route.js
+++ src/route.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./routes/ErrorPage";
+import LandingPage from "./routes/LandingPage";
 
 export default function RouteSwitcher() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<App />}>
+                    <Route index element={<LandingPage />}></Route>
                     <Route path={"*"} element={<ErrorPage />}></Route>
                 </Route>
             </Routes>
